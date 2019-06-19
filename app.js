a59894e10389a05086256e65f9b06c989fcf6bab
terrirp <terri.persad@gmail.com>
2019-06-19 12:33:17 -0400

    added responsive and focus state

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 
-function toggleNav() {
-   $('.sideNav').classList.toggle('active');
-}
+$(document).ready(function () {
+$('div.title').fadeIn(4000);
+
+   $('.hamburger').on('click', function () {
+      $('.menu').toggleClass('show');
+   });
+
+   $('.exit-responsive').on('click', function () {
+      $('.menu').toggleClass('show');
+   });
+   AOS.init();
+   
+});
